61a8febbae8b86b383c36e0b66be532494993dd2
Makdoom Shaikh <makshaikh99@gmail.com>
2021-10-11 08:22:01 -0700

    WIP in about section

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,28 +4,28 @@ import NavBar from "../NavBar/NavBar";
 import "./home.css";
 
 const Home = () => {
-  const circleText = document.querySelector('.circle-text');
-  circleText.innerHTML = circleText.innerText.split("")
-  circleText.innerHTML.map((character, index) => {
-    `<span>${character}</span>`
-  }).join("");
+  // const circleText = document.querySelector('.circle-text');
+  // circleText.innerHTML = circleText.innerText.split("")
+  // circleText.innerHTML.map((character, index) => {
+  //   `<span>${character}</span>`
+  // }).join("");
   return (
-      <div className="home-container">
-        <div className="navbar-container">
-          <NavBar />
+    <div className="home-container">
+      <div className="navbar-container">
+        <NavBar />
+      </div>
+      <div className="container">
+        <div className="hero-container">
+          <Hero />
         </div>
-        <div className="container">
-          <div className="hero-container">
-            <Hero />
-          </div>
-          <div className="scroll-more-container">
-            <div className="circle">
-              <div className="circle-text">
-                <p>Scroll for more</p>
-              </div>
+        {/* <div className="scroll-more-container">
+          <div className="circle">
+            <div className="circle-text">
+              <p>Scroll for more</p>
             </div>
           </div>
-        </div>
+        </div> */}
+      </div>
     </div>
   );
 };
