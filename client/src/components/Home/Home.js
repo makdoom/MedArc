802033d83d47c8888892e2294f5fdd213d87ae08
Makdoom Shaikh <swdsm100051@gmail.com>
2021-10-13 13:59:00 +0530

    modifying humburger

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,14 +4,6 @@ import NavBar from "../NavBar/NavBar";
 import "./home.css";
 
 const Home = () => {
-<<<<<<< HEAD
-=======
-  // const circleText = document.querySelector('.circle-text');
-  // circleText.innerHTML = circleText.innerText.split("")
-  // circleText.innerHTML.map((character, index) => {
-  //   `<span>${character}</span>`
-  // }).join("");
->>>>>>> c90f8c247d999a10a5a7ccca1a50ea955f316e10
   return (
     <div className="home-container">
       <div className="navbar-container">
@@ -21,21 +13,6 @@ const Home = () => {
         <div className="hero-container">
           <Hero />
         </div>
-<<<<<<< HEAD
-        <div className="scroll-more-container">
-=======
-        {/* <div className="scroll-more-container">
->>>>>>> c90f8c247d999a10a5a7ccca1a50ea955f316e10
-          <div className="circle">
-            <div className="circle-text">
-              <p>Scroll for more</p>
-            </div>
-          </div>
-<<<<<<< HEAD
-        </div>
-=======
-        </div> */}
->>>>>>> c90f8c247d999a10a5a7ccca1a50ea955f316e10
       </div>
     </div>
   );
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,38 +1,77 @@
 import React from "react";
 import "./navBar.css";
 
-import arc from '../../assets/images/arc.svg';
+import arc from "../../assets/images/arc.svg";
 
 const NavBar = () => {
-  
-  const navSlide = () => {
+  const handleNavbar = () => {
     const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-
-    nav.classList.toggle("nav-active")
-    burger.classList.toggle("active")
-  }
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+  };
   return (
-    <div className="container">
-    <nav>
-      <div className="logo">
-        <img className="logo-img" src={arc} alt="" />
-        <h4 className="logo-text">MedArc</h4>
-      </div>
-      <ul className="nav-links">
-        <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About Us</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Service</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Contact Us</a></li>
-      </ul>
-      <div className="burger" onClick={navSlide} >
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
-
+    <div className="container-fluid">
+      <nav>
+        <div className="logo">
+          <h3>MedArc</h3>
+        </div>
+        <ul className="nav-links active">
+          <li className="nav-link">
+            <a href="#">HOME</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">WHO WE ARE</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">WHAT WE DO</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">CONTACT US</a>
+          </li>
+        </ul>
+        <div className="burger" onClick={handleNavbar}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
     </div>
+
+    // <div className="container">
+    //   <nav>
+    //     <div className="logo">
+    //       <img className="logo-img" src={arc} alt="" />
+    //       <h4 className="logo-text">MedArc</h4>
+    //     </div>
+    //     <ul className="nav-links">
+    //       <li className="nav-item">
+    //         <a href="#" className="nav-link">
+    //           Home
+    //         </a>
+    //       </li>
+    //       <li className="nav-item">
+    //         <a href="#" className="nav-link">
+    //           About Us
+    //         </a>
+    //       </li>
+    //       <li className="nav-item">
+    //         <a href="#" className="nav-link">
+    //           Service
+    //         </a>
+    //       </li>
+    //       <li className="nav-item">
+    //         <a href="#" className="nav-link">
+    //           Contact Us
+    //         </a>
+    //       </li>
+    //     </ul>
+    //     <div className="burger" onClick={navSlide}>
+    //       <div className="line1"></div>
+    //       <div className="line2"></div>
+    //       <div className="line3"></div>
+    //     </div>
+    //   </nav>
+    // </div>
   );
 };
 
