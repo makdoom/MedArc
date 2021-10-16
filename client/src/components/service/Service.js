32af2bfb8c50d741db6f12651f176bc659b37a64
Makdoom Shaikh <swdsm100051@gmail.com>
2021-10-16 13:38:21 +0530

    WIP in service section

--- client/src/components/service/Service.js
+++ client/src/components/service/Service.js
@@ -1,50 +1,86 @@
 import React from "react";
-import about from "../../assets/images/about.png";
+import "./service.css";
+// import service from "../../assets/images/about.png";
 
 const Service = () => {
   return (
-    <div className="about-container">
+    <div className="service-container">
       <div className="container">
-        <div className="about-section">
+        <div className="service-section">
           <div className="title-section">
-            <h2 className="about-title">What We Do</h2>
+            <h2 className="service-title">What We Do</h2>
           </div>
 
           <div className="content-section">
             <div className="row align-items-center">
               <div className="col-md-6">
-                <div className="about-side-img">
-                  <img src={about} alt="about-img" />
-                  {/* <img src={doc} alt="about-img" /> */}
+                <div className="service-side-img">
+                  {/* <img src={about} alt="service-img" /  > */}
+                  {/* <img src={doc} alt="service-img" /> */}
                 </div>
               </div>
               <div className="col-md-6">
-                <div className="about-content">
-                  <p className="about-text">
-                    over the last decade, society has witnessed an explosion of
-                    health-related technology, from electronic health records to
-                    IoT fitness devices to low-cost whole genome sequencing.
-                    Currently, data from these sources is largely siloed – with
-                    limited or no integration with other data sources. At{" "}
-                    <span> MedArc</span>, we believe that the health industry is
-                    at a tipping point. Technologies now exist that will break
-                    down these silos and enable health data to be connected on a
-                    global scale. <br />
-                    <br />
-                    Healthcare will be predictive, preventative and personal.
-                    Healthcare will be accessible to every person, everywhere,
-                    regardless of who they are, where they are, . And with each
-                    new connection, our collective human intelligence will grow
-                    stronger. <br />
-                    <br />
-                    <span>MedArc</span> was founded with one mission: to enable
-                    this next era of health. We believe that each person
-                    deserves to live their healthiest, happiest life, and that
-                    data will democratize health on a both a global and an
-                    individual level.
-                    <br />
-                    <br />
+                <div className="service-content">
+                  <h3>WE BELIEVE</h3>
+                  <p className="service-text">
+                    Helping make the world a healthier place isn’t just a tag
+                    line, it’s our passion. We believe data holds the power to
+                    unleash our collective human intelligence. We believe that
+                    data can enable new medical breakthroughs, more intelligent
+                    health products, better coordination, and truly
+                    individualized care. And most importantly, we believe that
+                    individuals should be at the center of their own data
+                    universe, not the periphery. We believe that:
                   </p>
+                  <ul className="believe-list">
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">1</span>
+                        <p className="link-text">You should own your data</p>
+                      </div>
+                    </li>
+
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">2</span>
+                        <p className="link-text">
+                          You should have access to all your data
+                        </p>
+                      </div>
+                    </li>
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">3</span>
+                        <p className="link-text">
+                          You should have confidence that your data is safe
+                        </p>
+                      </div>
+                    </li>
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">4</span>
+                        <p className="link-text">
+                          You should be able to trust and understand your data
+                        </p>
+                      </div>
+                    </li>
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">5</span>
+                        <p className="link-text">
+                          You should be able to control who can access your data
+                        </p>
+                      </div>
+                    </li>
+                    <li className="believe-link">
+                      <div className="link-box">
+                        <span className="link-number">6</span>
+                        <p className="link-text">
+                          You should be able to help society with your data
+                        </p>
+                      </div>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
