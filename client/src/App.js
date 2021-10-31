6728b00989f4747603fe34c2ce34c485595736fa
Makdoom Shaikh <makshaikh99@gmail.com>
2021-10-31 04:32:24 -0700

    working in login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,13 @@
-import "./App.css";
-
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Service from "./components/service/Service";
-import Testimonial from "./components/testimonial/Testimonial";
-import Contact from "./components/contact/Contact";
+import Main from "./components/Main/Main";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="app-container">
-      <section className="home" id="home">
-        <Home />
-      </section>
-      <section className="about" id="about">
-        <About />
-      </section>
-      <div id="move-text-container">
-        <div className="move-text">
-          <span className="vision">Our Vision</span> <br />
-          <h1 className="main-head">make the world a healthier place... ! </h1>
-        </div>
-      </div>
-      <section className="Service" id="service">
-        <Service />
-      </section>
-      <section id="testimonials">
-        <Testimonial />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <footer>
-        <div className="design">
-          <p>
-            Design and Developed by{" "}
-            <a href="https://makdoom.github.io/">Makdoom Shaikh</a> With ❤️
-          </p>
-        </div>
-      </footer>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
   );
 }
 
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Home from "../Home/Home";
+import About from "../About/About";
+import Service from "../Service/Service";
+import Testimonial from "../Testimonial/Testimonial";
+import Contact from "../Contact/Contact";
+import "./main.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <section className="home" id="home">
+        <Home />
+      </section>
+      <section className="about" id="about">
+        <About />
+      </section>
+      <div id="move-text-container">
+        <div className="move-text">
+          <span className="vision">Our Vision</span> <br />
+          <h1 className="main-head">make the world a healthier place... ! </h1>
+        </div>
+      </div>
+      <section className="Service" id="service">
+        <Service />
+      </section>
+      <section id="testimonials">
+        <Testimonial />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <footer>
+        <div className="design">
+          <p>
+            Design and Developed by{" "}
+            <a href="https://makdoom.github.io/">Makdoom Shaikh</a> With ❤️
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
